1c14e89a78bbf784c4158b41943ebc84babfb6cc
Nigel van Keulen <nigel2392@gmail.com>
2024-02-21 22:33:27 +0100

    Update to package

--- wagtail_word/signals.py
+++ wagtail_word/signals.py
@@ -10,7 +10,7 @@ def create_default_collection(sender, **kwargs):
     if not root:
         root = Collection.add_root(name="Root")
         
-    c1, _ = Collection.objects.get_or_create(name=COLLECTION_NAME)
+    c1, _ = Collection.objects.get_or_create(name=COLLECTION_NAME, depth=1)
     
     if not root.get_children().filter(name=COLLECTION_NAME).exists():
         root.add_child(instance=c1)
